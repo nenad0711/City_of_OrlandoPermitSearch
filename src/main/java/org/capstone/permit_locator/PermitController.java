e6ddd2cee9c7df4e85e991106670e5d9c89926ba
nenad0711 <nenad0711@gmail.com>
2024-02-19 23:02:34 -0500

    templates and endpoints added

--- src/main/java/org/capstone/permit_locator/PermitController.java
+++ src/main/java/org/capstone/permit_locator/PermitController.java
@@ -5,102 +5,94 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class PermitController {
-	public Permit [] permits;
-	private static final Logger log = LoggerFactory.getLogger(PermitLocatorApplication.class);
-	@GetMapping ("/search")
-	public String searchPermits(
-			//@RequestParam(value = "address", required = false) String address,
-			//@RequestParam(value = "ownerName", required = false) String ownerName,
-			@RequestParam(value = "dropdown", defaultValue = "") String dropdown,
-			Model model) {
-		log.info(dropdown);
-		//System.out.println(dropdown);
-		// Construct the API URL based on the search parameters
-		String apiUrl = "https://data.cityoforlando.net/resource/ryhf-m453.json?";
-		/**if (address != null && !address.isEmpty()) {
-			apiUrl += "permit_address=" + address;
-		}
-		if (ownerName != null && !ownerName.isEmpty()) {
-			if (!apiUrl.endsWith("?")) {
-				apiUrl += "&";
-			}
-			apiUrl += "property_owner_name=" + ownerName;
-		}
-		if (dropdown != null && !dropdown.isEmpty()) {
-			if (!apiUrl.endsWith("?")) {
-				apiUrl += "&";
-			}**/
-			apiUrl += "application_type=" + dropdown;
+    public Permit[] permits;
+    private static final Logger log = LoggerFactory.getLogger(PermitLocatorApplication.class);
 
+    @GetMapping("/")
+    public String index(Model model) {
+        return "Home";
+    }
 
-		RestTemplate template = new RestTemplate();
-		ResponseEntity<Permit[]> response = template.getForEntity(apiUrl+"$limit=5", Permit[].class);
-		permits = response.getBody();
-		assert permits != null;
-		// Retrieve permit data from the API based on the search parameters
-		//List<Permit> permits = retrievePermitsFromApi(apiUrl);
-		model.addAttribute("permits", permits);
+    @GetMapping("/search")
+    public String searchPermits(@RequestParam(name = "address") String address,
+                                @RequestParam(name = "ownerName") String ownerName,
+                                @RequestParam(name = "permitType") String permitType,
+                                Model model) {
 
-		// Return the permits template to display the search results
-		return "Results";
-	}
+        log.info(address + " " + ownerName + " " + permitType);
+        String apiUrl = "https://data.cityoforlando.net/resource/ryhf-m453.json?";
+        boolean hasParameter = false;
+        if (address != null && !address.isEmpty()) {
+            apiUrl += "$where=permit_address like UPPER('" + address + "%')";
+            hasParameter = true;
+        }
+        if (ownerName != null && !ownerName.isEmpty()) {
+            if (hasParameter) {
+                apiUrl += " AND ";
+            } else {
+                apiUrl += "$where=";
+                hasParameter = true;
+            }
 
+            apiUrl += "property_owner_name like UPPER('" + ownerName + "%')";
+        }
+        if (permitType != null && !permitType.isEmpty()) {
+            if (hasParameter) {
+                apiUrl += " AND ";
+            }
+            apiUrl += "application_type='" + permitType + "'";
+        }
+        // Retrieve permit data from the API based on the search parameters
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<Permit[]> response = template.getForEntity(apiUrl, Permit[].class);
+        permits = response.getBody();
+        assert permits != null;
 
+        model.addAttribute("permits", permits);
 
+        // Return the template to display the search results
+        return "Results";
+    }
 
-/**
-	@GetMapping("/permits")
-	public String showPermits(Model model) {
-		//Permit [] permits = searchPermits();
-		model.addAttribute("permits", permits);
-		// Return the name of the HTML template to render
-		return "Results";
-	}**/
-	@GetMapping("/details/{id}")
-	public String getPermitDetails(@PathVariable String id, Model model) {
-		// Retrieve permit details for the given ID (from database, API, etc.)
-		Permit permit = retrievePermitDetails(id);
-		model.addAttribute("permit", permit);
-		return "Additional_Info";
-	}
-	public Permit retrievePermitDetails(String id) {
+    @GetMapping("/details/{id}")
+    public String getPermitDetails(@PathVariable String id, Model model) {
+        Permit permit = retrievePermitDetails(id);
+        model.addAttribute("permit", permit);
+        return "Additional_Info";
+    }
 
-		for (Permit perm: permits)
-			if(perm.permit_number().equals(id)){
-				return perm;
-			}
-		// Implementation to retrieve permit details for the given ID
-		// For simplicity, you can use mock permit details here
-		//return new Permit(id, "Permit " + id, id * 100);
+    public Permit retrievePermitDetails(String id) {
 
+        for (Permit perm : permits)
+            if (perm.permit_number().equals(id)) {
+                return perm;
+            }
         return null;
     }
-	/**
-	public Permit[] searchPermits() {
-		RestTemplate template = new RestTemplate();
-		String url  ="https://data.cityoforlando.net/resource/ryhf-m453.json?$limit=10";
-		ResponseEntity<Permit[]> response = template.getForEntity(url, Permit[].class);
-		Permit [] permits = response.getBody();
-		assert permits != null;
-		return permits;
-		Permit [] permitList = template.getForObject(url, Permit[].class);
-		for (Permit permit : permitList) {
-			System.out.println("Permit ID: " +   permit.permit_number());
-			System.out.println("Permit Application number: " + permit.application_type());
-			System.out.println("Permit Owner Name: " + permit.property_owner_name());
-			System.out.println("Permit Address: " + permit.permit_address());
-			System.out.println("Permit Worktype: " + permit.worktype());
-			System.out.println("Permit Status: " + permit.application_status());
-			System.out.println("------------------------------------------");
-		}
-		return permitList;
-	}**/
 }
+/**
+ public Permit[] searchPermits() {
+ RestTemplate template = new RestTemplate();
+ String url  ="https://data.cityoforlando.net/resource/ryhf-m453.json?$limit=10";
+ ResponseEntity<Permit[]> response = template.getForEntity(url, Permit[].class);
+ Permit [] permits = response.getBody();
+ assert permits != null;
+ return permits;
+ Permit [] permitList = template.getForObject(url, Permit[].class);
+ for (Permit permit : permitList) {
+ System.out.println("Permit ID: " +   permit.permit_number());
+ System.out.println("Permit Application number: " + permit.application_type());
+ System.out.println("Permit Owner Name: " + permit.property_owner_name());
+ System.out.println("Permit Address: " + permit.permit_address());
+ System.out.println("Permit Worktype: " + permit.worktype());
+ System.out.println("Permit Status: " + permit.application_status());
+ System.out.println("------------------------------------------");
+ }
+ return permitList;
+ }**/
+
--- src/main/java/org/capstone/permit_locator/PermitLocatorApplication.java
+++ src/main/java/org/capstone/permit_locator/PermitLocatorApplication.java
@@ -1,37 +1,13 @@
 package org.capstone.permit_locator;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class PermitLocatorApplication {
-
-	//private static final Logger log = LoggerFactory.getLogger(PermitLocatorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PermitLocatorApplication.class, args);
 	}
 
-/**
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	@Profile("!test")
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Permit permit = restTemplate.getForObject(
-					"http://localhost:8080/api/random", Permit.class);
-			log.info(permit.toString());
-		};
-	}**/
 }
